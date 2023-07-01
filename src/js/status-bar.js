3d6d2ba9a0202a805a3184e7436cd0ea6a559ec3
IQ <ilhomparisi@yandex.com>
2023-07-01 22:17:22 +0500

    Initial commit

---
+++ src/js/status-bar.js
@@ -0,0 +1,21 @@
+import { StatusBar , Style } from '@capacitor/status-bar';
+import { NavigationBar } from '@hugotomazi/capacitor-navigation-bar';
+
+
+
+NavigationBar.setColor({ color: '#000000' });
+// Make the status bar transparent and set the color mode to light
+// StatusBar.setTranslucent({
+//   translucent: true,
+//   colorScheme: 'light'
+// });
+// StatusBar.setOverlaysWebView({ overlay: true });
+
+StatusBar.setBackgroundColor({ color: '#000000' });
+// StatusBar.hide();
+
+// StatusBar.hide();
+
+// Hide the status bar
+// StatusBar.hide();
+
